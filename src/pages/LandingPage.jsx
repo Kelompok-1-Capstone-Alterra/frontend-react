434e265af3e778f4feb982d7fce40efa60a433e5
agusbw <agusbudhiarta81@gmial.com>
2023-05-16 23:14:41 +0800

    test git bw

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,3 +1,8 @@
 export default function LandingPage() {
-  return <h1 className="text-error">Ini halaman landingpage</h1>;
+  return (
+    <>
+      <h1 className="text-error">Ini halaman landingpage</h1>
+      <p>Landing Page test</p>
+    </>
+  );
 }

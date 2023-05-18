41caf7de7c99cf2e01168df7eb71315c5ea4df31
agusbw <agusbudhiarta81@gmial.com>
2023-05-18 22:48:47 +0800

    change padding

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -2,7 +2,7 @@ import Logo from "../../assets/Logo.png";
 
 export default function Footer() {
   return (
-    <footer className="flex items-center justify-between px-32 py-[18px] bg-white">
+    <footer className="flex items-center justify-between px-12 py-[18px] bg-white">
       <div className="flex items-center gap-2">
         <img src={Logo} className="w-10" />
         <p className="text-2xl font-bold text-primary">Agriplant</p>

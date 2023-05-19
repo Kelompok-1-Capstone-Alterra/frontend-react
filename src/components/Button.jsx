2f6b4c65797da90239699b0c6acab0fe35c6bb88
Tommy394 <67581158+Tommy394@users.noreply.github.com>
2023-05-19 23:11:33 +0700

    fix typo and change text button variant bg to transparent

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -12,8 +12,8 @@ export default function Button({
 			"border border-primary text-primary bg-transparent hover:bg-transparent hover:text-primary-hover hover:border-primary-hover active:border-primary-pressed active:text-primary-pressed disabled:text-neutral-40 disabled:border-neutral-20 disabled:hover:border-neutral-20 disabled:hover:text-neutral-40",
 		gray: "bg-neutral-20 hover:bg-neutral-30 text-neutral-40 border-none",
 		lightgreen:
-			"bg-primary-border text-primary hover:bg-primary hover:text-neutral-10 focus:bg-primary-surface isabled:bg-neutral-20 disabled:text-neutral-40 disabled:hover:bg-neutral-20 disabled:hover:text-neutral-40",
-		text: "text-primary border-none hover:text-primary-hover hover:bg-transparent active:text-primary-pressed disabled:text-neutral-40 disabled:hover:text-neutral-40",
+			"bg-primary-border text-primary hover:bg-primary hover:text-neutral-10 focus:bg-primary-surface disabled:bg-neutral-20 disabled:text-neutral-40 disabled:hover:bg-neutral-20 disabled:hover:text-neutral-40",
+		text: "text-primary border-none bg-transparent hover:text-primary-hover hover:bg-transparent active:text-primary-pressed disabled:text-neutral-40 disabled:hover:text-neutral-40",
 	};
 
 	const sizes = {

77fddc1a0fcfaf9911a6c7a0a39be1e63f03c30d
Tommy394 <67581158+Tommy394@users.noreply.github.com>
2023-05-19 23:01:00 +0700

    change outline and text button variant states style

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -9,11 +9,11 @@ export default function Button({
 		green:
 			"btn-primary text-neutral-10 hover:bg-primary-hover active:bg-primary-pressed disabled:bg-neutral-20 disabled:text-neutral-40 disabled:hover:bg-neutral-20 disabled:hover:text-neutral-40",
 		"outline-green":
-			"border border-primary text-primary hover:bg-primary-border focus:bg-primary-surface disabled:text-neutral-40 disabled:border-neutral-20 disabled:hover:border-neutral-20 disabled:hover:text-neutral-40",
+			"border border-primary text-primary bg-transparent hover:bg-transparent hover:text-primary-hover hover:border-primary-hover active:border-primary-pressed active:text-primary-pressed disabled:text-neutral-40 disabled:border-neutral-20 disabled:hover:border-neutral-20 disabled:hover:text-neutral-40",
 		gray: "bg-neutral-20 hover:bg-neutral-30 text-neutral-40 border-none",
 		lightgreen:
 			"bg-primary-border text-primary hover:bg-primary hover:text-neutral-10 focus:bg-primary-surface isabled:bg-neutral-20 disabled:text-neutral-40 disabled:hover:bg-neutral-20 disabled:hover:text-neutral-40",
-		text: "text-primary border-none hover:bg-primary-border disabled:text-neutral-40 disabled:hover:text-neutral-40 focus:bg-primary-surface",
+		text: "text-primary border-none hover:text-primary-hover hover:bg-transparent active:text-primary-pressed disabled:text-neutral-40 disabled:hover:text-neutral-40",
 	};
 
 	const sizes = {

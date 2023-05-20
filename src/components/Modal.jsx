8ff3941e8187c3220f7e94795f987381cdc42906
dhiasfa <salsabilamiqi@gmail.com>
2023-05-20 11:32:49 +0700

    add modal and card components

---
+++ src/components/Modal.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import { CheckmarkCircle32Regular, Info24Regular } from "@fluentui/react-icons";
+import Button from "./Button";
+
+const Modal = ({
+  isOpen,
+  text,
+  title,
+  cancelText,
+  confirmText,
+  icon,
+  onConfirm,
+  onCancel,
+}) => {
+  const handleConfirm = () => {
+    onConfirm();
+  };
+
+  const handleCancel = () => {
+    onCancel();
+  };
+
+  return (
+    <>
+      {isOpen && (
+        <div className="fixed inset-0 flex items-center justify-center z-50">
+          <div
+            className={`border bg-neutral-10 w-[487px] ${
+              icon ? "h-[312px]" : "h-64"
+            } rounded-[10px] p-[32px] shadow-elevation-2 border-1 top-1/2 left-1/2`}>
+            <div className="flex flex-col items-center justify-center">
+              {icon && icon === "success" && (
+                <CheckmarkCircle32Regular className="mb-3 w-10 h-10" />
+              )}
+              {icon && icon === "info" && (
+                <Info24Regular className="mb-3 w-10 h-10" />
+              )}
+              <h5 className="font-bold text-h-5 text-neutral-80 mb-2">
+                {title}
+              </h5>
+              <p className="font-normal text-center text-body-lg text-[#637381] mb-6">
+                {text}
+              </p>
+              <div className="modal-action space-x-4">
+                <Button
+                  variant={"outline-green"}
+                  className={"rounded-full w-[190px] h-[44px]"}
+                  onClick={handleCancel}>
+                  {cancelText ? cancelText : "Label"}
+                </Button>
+                <Button
+                  variant={"green"}
+                  className={"rounded-full w-[190px] h-[44px]"}
+                  onClick={handleConfirm}>
+                  {confirmText ? confirmText : "Label"}
+                </Button>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Modal;

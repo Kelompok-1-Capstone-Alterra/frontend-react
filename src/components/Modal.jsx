5b36f65bbd4b99ad3f08ccfc7767b07b1d8f7a26
dhiasfa <salsabilamiqi@gmail.com>
2023-05-20 16:55:56 +0700

    update modal with dynamic height and added icon

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,5 +1,9 @@
 import React from "react";
-import { CheckmarkCircle32Regular, Info24Regular } from "@fluentui/react-icons";
+import {
+  CheckmarkCircle24Regular,
+  Info24Regular,
+  Delete24Regular,
+} from "@fluentui/react-icons";
 import Button from "./Button";
 
 const Modal = ({
@@ -20,21 +24,21 @@ const Modal = ({
     onCancel();
   };
 
+  const iconComponents = {
+    success: CheckmarkCircle24Regular,
+    info: Info24Regular,
+    delete: Delete24Regular,
+  };
+
+  const SelectedIcon = icon && iconComponents[icon];
+
   return (
     <>
       {isOpen && (
         <div className="fixed inset-0 flex items-center justify-center z-50">
-          <div
-            className={`border bg-neutral-10 w-[487px] ${
-              icon ? "h-[312px]" : "h-64"
-            } rounded-[10px] p-[32px] shadow-elevation-2 border-1 top-1/2 left-1/2`}>
+          <div className="border bg-neutral-10 w-[487px] rounded-[10px] p-[32px] shadow-elevation-2 border-1 top-1/2 left-1/2 h-auto">
             <div className="flex flex-col items-center justify-center">
-              {icon && icon === "success" && (
-                <CheckmarkCircle32Regular className="mb-3 w-10 h-10" />
-              )}
-              {icon && icon === "info" && (
-                <Info24Regular className="mb-3 w-10 h-10" />
-              )}
+              {SelectedIcon && <SelectedIcon className="mb-3 w-10 h-10" />}
               <h5 className="font-bold text-h-5 text-neutral-80 mb-2">
                 {title}
               </h5>
@@ -43,14 +47,14 @@ const Modal = ({
               </p>
               <div className="modal-action space-x-4">
                 <Button
-                  variant={"outline-green"}
-                  className={"rounded-full w-[190px] h-[44px]"}
+                  variant="outline-green"
+                  className="rounded-full w-[190px] h-[44px]"
                   onClick={handleCancel}>
                   {cancelText ? cancelText : "Label"}
                 </Button>
                 <Button
-                  variant={"green"}
-                  className={"rounded-full w-[190px] h-[44px]"}
+                  variant="green"
+                  className="rounded-full w-[190px] h-[44px]"
                   onClick={handleConfirm}>
                   {confirmText ? confirmText : "Label"}
                 </Button>

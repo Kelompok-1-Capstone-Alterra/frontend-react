b477fc472e00bd559ad8f76a7929b652df7a2203
Tommy394 <67581158+Tommy394@users.noreply.github.com>
2023-05-21 15:11:26 +0700

    add change tab functionality

--- src/components/Tab.jsx
+++ src/components/Tab.jsx
@@ -1,14 +1,29 @@
-export default function Tab({ items }) {
+export default function Tab({
+  size = "md",
+  items,
+  activeTab,
+  widthFull = true,
+  vertical = false,
+}) {
+  const sizes = {
+    sm: "h-14",
+    md: "h-16",
+  };
+
   return (
-    <ul className="tabs gap-3 w-full justify-center">
+    <ul
+      className={`tabs gap-3 w-full justify-start ${
+        vertical ? "flex-col" : ""
+      }`}
+    >
       {items.map((item, index) => (
         <li
           key={index}
-          className={
-            "tab tab-bordered border-b-0 h-auto py-5 grow text-neutral-70/100 tab-active"
-          }
+          className={`tab tab-bordered border-b-0 px-3 text-neutral-70/100 ${
+            widthFull ? "grow" : ""
+          } ${sizes[size]} ${activeTab === index ? "tab-active" : ""}`}
         >
-          {item}
+          <span>{item}</span>
         </li>
       ))}
     </ul>

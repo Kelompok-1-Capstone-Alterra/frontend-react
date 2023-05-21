f345e9af63011b69133f4e042e718bd0ce0c0012
unknown <ivan.pradipta@ti.ukdw.ac.id>
2023-05-21 11:42:26 +0700

    add text area and field

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,21 +1,11 @@
-import TextArea from "../components/TextArea";
-import TextField from "../components/TextField";
-
-
 export default function Login() {
-
 
   return (
     <>
     <div className="pl-5">
-
       <h1>Ini halaman login</h1>
       <p>ini ivan </p>
       <p>ini footernya</p>
-
-      
-      <TextField label="nama" variant="green"></TextField>
-      <TextArea label = "Name" variant="green"/>
     </div>
     </>
   );

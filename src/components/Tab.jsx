231b4c0fe5d6b93e9ab20cb10fd90a65c0562ae6
Tommy394 <67581158+Tommy394@users.noreply.github.com>
2023-05-21 11:07:08 +0700

    add base tabs

--- src/components/Tab.jsx
+++ src/components/Tab.jsx
@@ -1,3 +1,9 @@
 export default function Tab() {
-  return <h1>Tab</h1>;
+  return (
+    <div className="tabs">
+      <a className="tab tab-bordered">Tab 1</a>
+      <a className="tab tab-bordered tab-active">Tab 2</a>
+      <a className="tab tab-bordered">Tab 3</a>
+    </div>
+  );
 }

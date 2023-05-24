e055e3e6d20f8c54b5e986c1cad225e30c95add2
Tommy394 <67581158+Tommy394@users.noreply.github.com>
2023-05-24 13:59:55 +0700

    rework tab to be able to display content (#26)

--- src/components/Tab.jsx
+++ src/components/Tab.jsx
@@ -1,7 +1,7 @@
 export default function Tab({
   size = "md",
-  items,
-  activeTab,
+  tabs = [],
+  activeTabIndex = 0,
   widthFull = true,
   vertical = false,
 }) {
@@ -11,21 +11,24 @@ export default function Tab({
   };
 
   return (
-    <ul
-      className={`tabs gap-3 w-full justify-start ${
-        vertical ? "flex-col" : ""
-      }`}
-    >
-      {items.map((item, index) => (
-        <li
-          key={index}
-          className={`tab tab-bordered border-b-0 px-3 text-neutral-70/100 pointer-events-none ${
-            widthFull ? "grow" : ""
-          } ${sizes[size]} ${activeTab === index ? "tab-active" : ""}`}
-        >
-          <span>{item}</span>
-        </li>
-      ))}
-    </ul>
+    <div>
+      <ul
+        className={`tabs gap-3 w-full justify-start ${
+          vertical ? "flex-col" : ""
+        }`}
+      >
+        {tabs?.map((tab, index) => (
+          <li
+            key={tab.id}
+            className={`tab tab-bordered border-b-0 px-3 text-neutral-70/100 ${
+              widthFull ? "grow" : ""
+            } ${sizes[size]} ${activeTabIndex === index ? "tab-active" : ""}`}
+          >
+            <span>{tab.title}</span>
+          </li>
+        ))}
+      </ul>
+      <div>{tabs[activeTabIndex]?.content}</div>
+    </div>
   );
 }

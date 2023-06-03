ff1821b3dcda2072ee2eb5f82c2615b6af2e53e8
Tommy394 <67581158+Tommy394@users.noreply.github.com>
2023-06-03 15:10:43 +0700

    finish detail plant page

---
+++ src/pages/DetailPlantPage.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import Tab from "../components/Tab";
+import SecondaryContainer from "../components/layouts/SecondaryContainer";
+import DetailTanamanView from "../components/detailPlantPage/DetailTanamanView";
+import PenanamanView from "../components/detailPlantPage/PenanamanView";
+import PemupukkanView from "../components/detailPlantPage/PemupukkanView";
+import PenyiramanView from "../components/detailPlantPage/PenyiramanView";
+import TemperaturView from "../components/detailPlantPage/TemperaturView";
+
+export default function DetailPlantPage() {
+  const [activeTabIndex, setActiveTabIndex] = useState(0);
+
+  const tabs = [
+    {
+      id: "detailTanaman",
+      title: "Detail Tanaman",
+      content: <DetailTanamanView />,
+    },
+    {
+      id: "penanaman",
+      title: "Penanaman",
+      content: <PenanamanView />,
+    },
+    {
+      id: "pemupukkan",
+      title: "Pemupukkan",
+      content: <PemupukkanView />,
+    },
+    {
+      id: "penyiraman",
+      title: "Penyiraman",
+      content: <PenyiramanView />,
+    },
+    {
+      id: "temperatur",
+      title: "Temperatur",
+      content: <TemperaturView />,
+    },
+  ];
+
+  return (
+    <SecondaryContainer title="Data Tanaman">
+      <div className="ps-10">
+        <Tab
+          tabs={tabs}
+          setActiveTabIndex={setActiveTabIndex}
+          activeTabIndex={activeTabIndex}
+        />
+      </div>
+    </SecondaryContainer>
+  );
+}

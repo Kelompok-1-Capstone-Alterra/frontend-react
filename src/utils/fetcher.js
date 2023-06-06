40e6d7e783f1b624df01893fa7a6fe416b904382
agusbw <agusbudhiarta81@gmial.com>
2023-06-06 17:26:04 +0800

    change fetcher function

--- src/utils/fetcher.js
+++ src/utils/fetcher.js
@@ -1,5 +1,14 @@
 import axios from "axios";
+import Cookies from "js-cookie";
 
-const fetcher = (url) => axios.get(url).then((res) => res.data);
+const token = Cookies.get("token");
+const headers = token ? { Authorization: `Bearer ${token}` } : {};
+
+const fetcher = (url) =>
+  axios
+    .get(url, {
+      headers: headers,
+    })
+    .then((res) => res.data);
 
 export default fetcher;

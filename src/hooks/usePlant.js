bd390853552064dc61b8f68184bd94acfa1aa629
Tommy394 <67581158+Tommy394@users.noreply.github.com>
2023-06-08 21:45:30 +0700

    create a custom useplant custom hook for create and delete

---
+++ src/hooks/usePlant.js
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import axios from "axios";
+import Cookies from "js-cookie";
+
+const usePlant = () => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const deletePlant = async (id) => {
+    setIsLoading(true);
+    try {
+      const response = await axios.delete(
+        `${import.meta.env.VITE_API_BASE_URL}/auth/admins/plants/${id}/detail`,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${Cookies.get("token")}`,
+          },
+        }
+      );
+
+      return response;
+    } catch (error) {
+      console.log(error);
+      return error.response;
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const createPlant = async (body) => {
+    try {
+      const res = await axios.post(
+        `${import.meta.env.VITE_API_BASE_URL}/auth/admins/plants/add`,
+        body,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${Cookies.get("token")}`,
+          },
+        }
+      );
+      return res;
+    } catch (error) {
+      return error.response;
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return {
+    isLoading,
+    deletePlant,
+    createPlant,
+  };
+};
+
+export default usePlant;

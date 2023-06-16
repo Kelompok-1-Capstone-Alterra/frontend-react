79ac9ce06713b36039d3e79a3facb208d8fdb0d5
Tommy394 <67581158+Tommy394@users.noreply.github.com>
2023-06-16 22:08:25 +0700

    Add loading state plant (#76)
    * add save as draft feature
    
    * remove padding
    
    * add helpers function for draft feature
    
    * add recoil effect to initialize atom to local storage if exist
    
    * add on click event on tambah button to reset state, fix picture src and modal confirm text
    
    * change loading text with loading component
    
    * add loading state when page load
    
    * add swr and loading state
    
    * delete update and detail plant loader
    
    * add a get image function in custom hooks
    
    * refactor get image to use custom hook, add loading state to wait image downloading and disabled button when submitting
    
    * add loading state to disabled button when submitting

--- src/pages/DetailPlantPage.jsx
+++ src/pages/DetailPlantPage.jsx
@@ -1,5 +1,7 @@
-import { useLoaderData } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { useState } from "react";
+import useSWR from "swr";
+import Cookies from "js-cookie";
 
 import Tab from "../components/Tab";
 import SecondaryContainer from "../components/layouts/SecondaryContainer";
@@ -8,10 +10,20 @@ import PenanamanView from "../components/detailPlantPage/PenanamanView";
 import PemupukkanView from "../components/detailPlantPage/PemupukkanView";
 import PenyiramanView from "../components/detailPlantPage/PenyiramanView";
 import TemperaturView from "../components/detailPlantPage/TemperaturView";
+import fetcher from "../utils/fetcher";
+import Loading from "../components/Loading";
 
 export default function DetailPlantPage() {
-  const plants = useLoaderData();
-  console.log(plants);
+  // const plants = useLoaderData();
+  const param = useParams();
+  const { data, isLoading } = useSWR(
+    `${import.meta.env.VITE_API_BASE_URL}/auth/admins/plants/${
+      param.id
+    }/detail`,
+    (url) => fetcher(url, Cookies.get("token"))
+  );
+  const plant = data?.data;
+  console.log(plant);
   const [activeTabIndex, setActiveTabIndex] = useState(0);
 
   const tabs = [
@@ -21,10 +33,10 @@ export default function DetailPlantPage() {
       content: (
         <DetailTanamanView
           plantDetail={{
-            plant_name: plants?.plant_name,
-            plant_latin: plants?.plant_latin,
-            plant_description: plants?.plant_description,
-            plant_pictures: plants?.plant_pictures,
+            plant_name: plant?.plant_name,
+            plant_latin: plant?.plant_latin,
+            plant_description: plant?.plant_description,
+            plant_pictures: plant?.plant_pictures,
           }}
         />
       ),
@@ -32,24 +44,31 @@ export default function DetailPlantPage() {
     {
       id: "penanaman",
       title: "Penanaman",
-      content: <PenanamanView plantPlanting={plants?.planting_info} />,
+      content: <PenanamanView plantPlanting={plant?.planting_info} />,
     },
     {
       id: "pemupukkan",
       title: "Pemupukkan",
-      content: <PemupukkanView plantFertilizing={plants?.fertilizing_info} />,
+      content: <PemupukkanView plantFertilizing={plant?.fertilizing_info} />,
     },
     {
       id: "penyiraman",
       title: "Penyiraman",
-      content: <PenyiramanView plantWatering={plants?.watering_info} />,
+      content: <PenyiramanView plantWatering={plant?.watering_info} />,
     },
     {
       id: "temperatur",
       title: "Temperatur",
-      content: <TemperaturView plantTemperature={plants?.temperature_info} />,
+      content: <TemperaturView plantTemperature={plant?.temperature_info} />,
     },
   ];
+
+  if (isLoading)
+    return (
+      <div className="h-screen w-full flex justify-center items-center">
+        <Loading />
+      </div>
+    );
 
   return (
     <SecondaryContainer

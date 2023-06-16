74d32aaaf4afe37cb3f971860c340791c49c8622
agusbw <agusbudhiarta81@gmial.com>
2023-06-17 00:04:05 +0800

    fix navigate url

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -6,19 +6,23 @@ const axiosInstance = axios.create();
 axiosInstance.interceptors.response.use(
   (response) => response,
   (error) => {
-    console.log(error.response);
+    if (error.response && error.response.status === 504) {
+      router.navigate("/error504");
+    }
     if (error.response && error.response.status === 500) {
       router.navigate("/error500");
     }
     if (error.response && error.response.status === 401) {
-      router.navigate("/error401");
+      router.navigate("/error401", {
+        replace: true,
+      });
     }
     if (error.response && error.response.status === 403) {
-      router.navigate("/error403");
+      router.navigate("/error403", {
+        replace: true,
+      });
     }
-    if (error.response && error.response.status === 404) {
-      router.navigate("/error404");
-    }
+
     return Promise.reject(error);
   }
 );

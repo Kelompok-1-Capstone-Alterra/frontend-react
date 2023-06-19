de657564c66d7be7a37fc85279bf127117dfb07c
Tommy394 <67581158+Tommy394@users.noreply.github.com>
2023-06-19 21:52:44 +0700

    fix loading skeleton width

--- src/pages/OverviewPage.jsx
+++ src/pages/OverviewPage.jsx
@@ -34,7 +34,7 @@ export default function OverviewPage() {
                 }}
               ></AnimatedNumbers>
             ) : (
-              <Skeleton />
+              <Skeleton width={40} />
             )}
           </div>
         </div>

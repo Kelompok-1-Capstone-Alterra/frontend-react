806aeaae857d03d5e461009845da950f086d8df9
Tommy394 <67581158+Tommy394@users.noreply.github.com>
2023-06-24 21:23:45 +0700

    update download link (#130)

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-export const APPLICATION_DOWNLOAD_LINK = "#";
+export const APPLICATION_DOWNLOAD_LINK = "https://drive.google.com/file/d/1Nv4jTemnmDjF63LrQiMJnLU9F3EiJz4C/view";
 
 //react-quick module
 export const MODULES = {
